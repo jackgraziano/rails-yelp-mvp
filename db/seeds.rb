818b9c80c0631abce3784a1d80e81deb77978cd8
Jackson Graziano <jackgraziano@gmail.com>
2017-04-12 12:10:55 -0300

    initial restaurants list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  rest = Restaurant.new
+  rest.name = Faker::Hipster.words(3).join(" ").capitalize
+  rest.address = Faker::Address.street_address
+  rest.phone = Faker::PhoneNumber.phone_number
+  rest.category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  rest.save
+end
